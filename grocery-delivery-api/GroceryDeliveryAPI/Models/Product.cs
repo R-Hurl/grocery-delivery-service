3f86949bf1db56d23cf196535831e7ce245d4962
Ryan Hurley <ryan.hurley1212@gmail.com>
2021-11-13 19:19:21 -0600

    Added Price property to Product Model

--- grocery-delivery-api/GroceryDeliveryAPI/Models/Product.cs
+++ grocery-delivery-api/GroceryDeliveryAPI/Models/Product.cs
@@ -6,5 +6,6 @@ namespace GroceryDeliveryAPI.Models
         public int CategoryId { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
     }
 }
--- grocery-delivery-api/GroceryDeliveryAPI/Repositories/ProductRepository.cs
+++ grocery-delivery-api/GroceryDeliveryAPI/Repositories/ProductRepository.cs
@@ -16,42 +16,48 @@ namespace GroceryDeliveryAPI.Repositories
                 ID = 1,
                 CategoryId = 1,
                 Name = "Milk",
-                Description = "Stuff you put in cereal"
+                Description = "Stuff you put in cereal",
+                Price = 3.50m
             },
             new Product
             {
                 ID = 2,
                 CategoryId = 1,
                 Name = "Yogurt",
-                Description = "Fermented Milk"
+                Description = "Fermented Milk",
+                Price = 1.99m
             },
             new Product
             {
                 ID = 3,
                 CategoryId = 1,
                 Name = "Sour Cream",
-                Description = "Sour Fermented Milk"
+                Description = "Sour Fermented Milk",
+                Price = 1.20m
             },
             new Product
             {
                 ID = 4,
                 CategoryId = 1,
                 Name = "Cheddar Cheese",
-                Description = "Solid Milk"
+                Description = "Solid Milk",
+                Price = 2.00m
             },
             new Product
             {
                 ID = 5,
                 CategoryId = 1,
                 Name = "Swiss Cheese",
-                Description = "It's Swiss"
+                Description = "It's Swiss",
+                Price = 2.00m
             },
             new Product
             {
                 ID = 6,
                 CategoryId = 2,
                 Name = "Ground Beef",
-                Description = "Comes from cows"
+                Description = "Comes from cows",
+                Price = 6.80m
             },
         };
         public Task<IEnumerable<Product>> GetProductsByCategoryAsync(short categoryId)
