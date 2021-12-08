14a01592546bcffd6a3e732b1329e6de86c2f832
Ryan Hurley <ryan.hurley1212@gmail.com>
2021-12-07 18:04:08 -0600

    Added CSS and started form validation for Grocery Checkout Component

--- grocery-delivery-frontend/src/app/components/grocery-checkout/grocery-checkout.component.ts
+++ grocery-delivery-frontend/src/app/components/grocery-checkout/grocery-checkout.component.ts
@@ -9,6 +9,7 @@ import {
 import { CartEntity } from '../../reducers/cart.reducer';
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 import { removeFromCart, updateCartItem } from 'src/app/actions/cart.actions';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-grocery-checkout',
@@ -19,8 +20,31 @@ export class GroceryCheckoutComponent implements OnInit {
   cart$!: Observable<CartEntity[]>;
   total$!: Observable<number>;
   faTrashAlt = faTrashAlt;
+  checkoutForm = this.formBuilder.group({
+    firstName: ['', [Validators.required]],
+    lastName: ['', [Validators.required]],
+    address: this.formBuilder.group({
+      street: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      state: ['', [Validators.required, Validators.maxLength(2)]],
+      zip: ['', [Validators.required, Validators.maxLength(5)]],
+    }),
+  });
 
-  constructor(private store: Store<AppState>) {}
+  get firstName() {
+    return this.checkoutForm.get('firstName');
+  }
+  get lastName() {
+    return this.checkoutForm.get('lastName');
+  }
+  get street() {
+    return this.checkoutForm.get(['address', 'street']);
+  }
+
+  constructor(
+    private store: Store<AppState>,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.cart$ = this.store.select(selectCartEntities);
@@ -46,5 +70,10 @@ export class GroceryCheckoutComponent implements OnInit {
     };
 
     this.store.dispatch(updateCartItem({ payload }));
+  }
+
+  submit() {
+    console.warn(this.checkoutForm.value);
+    console.log(this.street);
   }
 }
