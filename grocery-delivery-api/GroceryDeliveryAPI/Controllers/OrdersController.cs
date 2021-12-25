895a807792b4cd91c919ffe8a12dbafc500a46c4
Ryan Hurley <ryan.hurley1212@gmail.com>
2021-12-24 22:38:39 -0700

    Finished producer logic of OrdersController that writes to "orders" kafka topic

--- grocery-delivery-api/GroceryDeliveryAPI/Controllers/OrdersController.cs
+++ grocery-delivery-api/GroceryDeliveryAPI/Controllers/OrdersController.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Threading.Tasks;
+using Confluent.Kafka;
 using GroceryDeliverAPI.Models;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace GroceryDeliverAPI.Controllers
 {
@@ -9,10 +11,40 @@ namespace GroceryDeliverAPI.Controllers
     [Route("api/[controller]")]
     public class OrdersController : Controller
     {
+        private readonly string KAFKA_TOPIC = "orders";
+
         [HttpPost]
         public async Task<ActionResult<Guid>> SubmitOrder([FromBody] Order order)
         {
+            // Generate order number.
+            var orderNumber = Guid.NewGuid();
+
             // TODO: Add Logic to produce messages to Kafka.
+            var producerConfig = new ProducerConfig
+            {
+                BootstrapServers = "localhost:9092",
+            };
+
+            try
+            {
+                using (var producer = new ProducerBuilder<string, string>(producerConfig).Build())
+                {
+                    var message = new Message<string, string>
+                    {
+                        Key = orderNumber.ToString(),
+                        Value = JsonConvert.SerializeObject(order)
+                    };
+
+                    // Write to ORDERS kafka topic
+                    producer.Produce(KAFKA_TOPIC, message);
+
+                    producer.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
 
             return await Task.FromResult(Guid.NewGuid());
         }
