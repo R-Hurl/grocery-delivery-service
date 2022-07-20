d6607f75db111d2265fd0db57d769c83fafcc685
Ryan Hurley <ryan.hurley1212@gmail.com>
2022-07-20 12:01:17 -0500

    Upgraded GroceryDeliveryAPI to .NET 6

--- grocery-delivery-api/GroceryDeliveryAPI/Program.cs
+++ grocery-delivery-api/GroceryDeliveryAPI/Program.cs
@@ -1,26 +1,81 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using GroceryDeliveryAPI.Profiles;
+using GroceryDeliveryAPI.Repositories;
+using GroceryDeliveryAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
-namespace GroceryDeliveryAPI
+namespace GroceryDeliveryAPI;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
+        builder.Services.RegisterMiddleware(configuration);
+        builder.Services.RegisterDependencies();
+
+        var app = builder.Build();
+
+        // Middleware Configuration
+        if (builder.Environment.IsDevelopment())
         {
-            CreateHostBuilder(args).Build().Run();
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "GroceryDeliveryAPI v1"));
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseCors(options =>
+        {
+            options.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+        });
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+
+        app.Run();
+    }
+}
+
+internal static class ProgramBuilderExtensions
+{
+    internal static IServiceCollection RegisterMiddleware(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddCors();
+        services.AddAutoMapper(typeof(OrderProfile), typeof(ProductProfile));
+
+        // Entity Framework Core
+        services.AddDbContext<GroceryDeliveryServiceContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("GroceryDeliveryServiceDB"));
+        });
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "GroceryDeliveryAPI", Version = "v1" });
+        });
+
+        return services;
+    }
+
+    internal static IServiceCollection RegisterDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        return services;
     }
 }
