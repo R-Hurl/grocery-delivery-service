d718e34d5b925093e205d0ca9db46d5c16af836a
Ryan Hurley <ryan.hurley1212@gmail.com>
2022-08-01 18:39:12 -0500

    Upgrade DeliverySchedulerConsumer to .NET 6

--- delivery-scheduler-consumer/DeliverySchedulerConsumer/Program.cs
+++ delivery-scheduler-consumer/DeliverySchedulerConsumer/Program.cs
@@ -1,26 +1,81 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Confluent.Kafka;
+using DeliverySchedulerConsumer.Repositories;
+using GroceryDeliveryAPI;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace DeliverySchedulerConsumer
+namespace DeliverySchedulerConsumer;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+        var configuration = builder.Configuration;
+        builder.Services.RegisterMiddleware(configuration);
+        builder.Services.RegisterDependencies();
+
+        var app = builder.Build();
+
+        if (builder.Environment.IsDevelopment())
         {
-            CreateHostBuilder(args).Build().Run();
+            app.UseDeveloperExceptionPage();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        app.UseRouting();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapGet("/", async context =>
+            {
+                await context.Response.WriteAsync("Hello World!");
+            });
+        });
+
+        app.Run();
+    }
+}
+
+internal static class ProgramBuilderExtensions
+{
+    internal static IServiceCollection RegisterMiddleware(this IServiceCollection services, IConfiguration configuration)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "broker:29092",
+            EnableAutoCommit = false,
+            GroupId = "delivery-scheduler-consumer",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        var consumer = new ConsumerBuilder<string, string>(config).Build();
+        consumer.Subscribe("postgres_db.public.orders");
+
+        // Entity Framework Core
+        services.AddDbContext<DeliverySchedulerDbContext>(options =>
+        {
+            options.UseNpgsql(configuration.GetConnectionString("GroceryDeliveryServiceDB"));
+        });
+
+        services.AddHostedService<Worker>(sp => new Worker(
+            consumer,
+            sp.GetRequiredService<ILogger<Worker>>(),
+            sp
+        ));
+
+        return services;
+    }
+
+    internal static IServiceCollection RegisterDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<IOrderRepository, OrderRepository>();
+
+        return services;
     }
 }
